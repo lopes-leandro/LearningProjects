a6ea296c561219da3dba200bd2e79eecb7cbcc7b
Leandro Lopes <lopesleandro.it@gmail.com>
2022-01-08 19:26:32 -0300

    configuração do novo faveicon

--- Angular/lemon-mart-app/src/app/app.component.ts
+++ Angular/lemon-mart-app/src/app/app.component.ts
@@ -4,8 +4,9 @@ import { Component } from '@angular/core';
   selector: 'app-root',
   template: `
     <mat-toolbar color="primary">
-      <a mat-button routerLink="/home"><h1>{{title}}</h1></a>      
+      <a mat-button routerLink="/home"><h1>{{title}}</h1></a>            
     </mat-toolbar>
+    <div>Icons made by <a href="https://www.flaticon.com/authors/justicon" title="justicon">justicon</a> from <a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a></div>
     <router-outlet></router-outlet>
   `,
 })
